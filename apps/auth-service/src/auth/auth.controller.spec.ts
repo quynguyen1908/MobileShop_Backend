8861486f6ac95e4b8de6879c9fc3c8c4c8c396ba
Nguyen Ngoc Quy <156199238+quynguyen1908@users.noreply.github.com>
2025-09-24 16:16:14 +0700

    fix test file

--- apps/auth-service/src/auth/auth.controller.spec.ts
+++ apps/auth-service/src/auth/auth.controller.spec.ts
@@ -1,15 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { createMock } from '@golevelup/ts-jest';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: createMock<AuthService>(),
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
--- apps/user-service/src/user/user.controller.spec.ts
+++ apps/user-service/src/user/user.controller.spec.ts
@@ -1,15 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { createMock } from '@golevelup/ts-jest';
 
 describe('UserController', () => {
   let controller: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+            provide: UserService,
+            useValue: createMock<UserService>(),
+        }
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
