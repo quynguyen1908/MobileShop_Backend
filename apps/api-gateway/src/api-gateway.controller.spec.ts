e5c1746bf09f08d2da5445f87b5813b09abcf576
Nguyen Ngoc Quy <156199238+quynguyen1908@users.noreply.github.com>
2025-09-26 13:40:49 +0700

    fix test file

--- apps/api-gateway/src/api-gateway.controller.spec.ts
+++ apps/api-gateway/src/api-gateway.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './api-gateway.controller';
+import { CircuitBreakerModule } from './circuit-breaker/circuit-breaker.module';
 
 describe('ApiGatewayController', () => {
   let controller: HealthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CircuitBreakerModule],
       controllers: [HealthController],
     }).compile();
 
